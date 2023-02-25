7128231c6567bc368b128292a81dd4e9a794d71a
FakhreevDS <fahreevds@gmail.com>
2023-02-26 02:28:01 +0300

    Пополнение счет_2

--- src/pages/UpdateAccount.jsx
+++ src/pages/UpdateAccount.jsx
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";
 import PostService from "../API/PostService";
-import AccountList from "../components/AccountList";
 import '../styles/App.css'
 
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import CurrencyRubleIcon from '@mui/icons-material/CurrencyRuble';
+import Button from '@mui/material/Button';
+
 
 function UpdateAccount() {
     const [accounts, setAccounts] = useState([]);
+    const [selected, setSelected] = useState('');
 
     //загрузка счетов при загрузке страницы
     useEffect(() => {
-      fetchAccounts()
+        fetchAccounts()
     }, [])
-  
-  
+
+
     // получить список открытых счетов
     async function fetchAccounts() {
-      const response = await PostService.getAll();
-      setAccounts(response)
-      console.log(accounts)
-      console.log(accounts)
+        const response = await PostService.getAll();
+        setAccounts(response)
     }
-  
-    return (
-        <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={accounts}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Accounts" />}
-      />
 
-    //   <div className="app">
-    //     {accounts.length
-    //       ? <AccountList list={accounts} />
-    //       : <span style={{ textAlign: 'center' }}>Нет активных счетов</span>
-    //     }
-    //   </div>
+    return (
+        <div className="up_account">
+            <div className="up_account_form">
+                <Autocomplete
+                    disablePortal
+                    disableClearable={true}
+                    id="combo-box-demo"
+                    options={accounts}
+                    getOptionLabel={(param) => param.name}
+                    sx={{ width: 300 }}
+                    onChange={(event, value) => setSelected(value)}
+                    renderInput={(params) => <TextField {...params} label="Номер счета" />}
+                />
+                <div className="balance">
+                    <div>{selected.id}</div>
+                    <div><CurrencyRubleIcon fontSize="small" /></div>
+                </div>
+            </div>
+            <div className="up_account_btn">
+                <div>
+                    <TextField id="standard-basic" label="Сумма" variant="standard" />
+                </div>
+                <div>
+                    <Button variant="contained">Пополнить</Button>
+                </div>
+            </div>
+        </div>
     );
 }
 
