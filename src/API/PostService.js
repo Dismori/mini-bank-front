57944ad9f626359bb018317a4da713aa9fd476a0
FakhreevDS <fahreevds@gmail.com>
2023-03-08 18:16:19 +0300

    port changes

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -17,7 +17,7 @@ export default class PostService {
     //получить все счета и баланс по счетам клиента
     static async getAll() {
         const clientIdValue = localStorage.getItem('clientId')
-        const response = await axios.get('http://localhost:8080/getAccountsInfo', {
+        const response = await axios.get('http://localhost:3001/getAccountsInfo', {
             headers: { 'clientId': clientIdValue }
         })
         console.log('getAll', response)
@@ -27,7 +27,7 @@ export default class PostService {
     //создать счет
     static createAccount(clientId) {
         // const clientIdValue = localStorage.getItem('clientId')
-        const response = axios.post('http://localhost:8080/create', {}, {
+        const response = axios.post('http://localhost:3001/create', {}, {
             headers: { 'clientId': clientId }
         })
         console.log('create', response)
@@ -36,7 +36,7 @@ export default class PostService {
     //закрыть счет
     static closeAccount(id) {
         const clientIdValue = localStorage.getItem('clientId')
-        const response = axios.delete('http://localhost:8080/delete', {
+        const response = axios.delete('http://localhost:3001/delete', {
             headers: { 'accountId': id, 'clientId': clientIdValue }
         })
 
@@ -47,7 +47,7 @@ export default class PostService {
     //пополнить баланс
     static updateAccount(id, sum) {
         const clientIdValue = localStorage.getItem('clientId')
-        const response = axios.put('http://localhost:8080/deposit', {
+        const response = axios.put('http://localhost:3001/deposit', {
             accountId: id,
             amount: sum
         }, {
@@ -58,7 +58,7 @@ export default class PostService {
 
     //зарегистрировать клиента
     static clientRegistration(message) {
-        const response = axios.post('http://localhost:8080/registre', message)
+        const response = axios.post('http://localhost:8080/client', message)
         console.log('registre', response)
         return response;
     }
@@ -67,7 +67,7 @@ export default class PostService {
     static intTransfer(accountFrom, accountTo, sum) {
         const clientIdValue = localStorage.getItem('clientId')
         console.log(accountFrom, accountTo, sum)
-        axios.put('http://localhost:8080/transfer', {
+        axios.put('http://localhost:3001/transfer', {
             accountIdFrom: accountFrom,
             accountIdTo: accountTo,
             amount: sum
